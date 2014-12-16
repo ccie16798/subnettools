ae866e014012da9c9f0170bb2b66401ff91eec03
etienne basset <etienne.basset@numericable.fr>
2014-12-16 18:42:43 +0100

    add header file for st_printf

--- st_printf.h
+++ st_printf.h
@@ -1,16 +1,12 @@
 #ifndef ST_PRINTF_H
 #define ST_PRINTF_H
 
-
-
-
-
 /* simple and dummy print a route */
 void fprint_route(const struct route *r, FILE *output, int compress_level);
 /* print a route 'r' according to format 'fmt' into output */
 void fprint_route_fmt(const struct route *r, FILE *output, const char *fmt);
 
-/* subnettool variants of sprintf, fprintf and printf 
+/* subnettool variants of sprintf, fprintf and printf
  * fmt understand the following types :
  *
  * %I : an IP (v4 or v6)   input variable is struct subnet *
