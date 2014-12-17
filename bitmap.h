3424ed83aeaafdf6a396fd6afdb571cde1a403f6
etienne basset <etienne.basset@numericable.fr>
2014-12-17 19:19:53 +0100

    update bitmap.h header
    Signed-off-by: etienne basset <etienne.basset@numericable.fr>

--- bitmap.h
+++ bitmap.h
@@ -1,8 +1,15 @@
 #ifndef BITMAP_H
 #define BITMAP_H
 
-void shift_right(unsigned  short *buffer, int len, int shift);
-void shift_left(unsigned  short *buffer, int len, int shift);
+#define MYTYPE    unsigned short
+#define TYPE_SIZE (sizeof(MYTYPE) * 8)
+#define TYPE_MAX  ((MYTYPE)-1)
+
+void shift_right(MYTYPE *buffer, int len, int shift);
+void shift_left(MYTYPE *buffer, int len, int shift);
+
+void decrease_bitmap(MYTYPE *buffer, int len);
+void increase_bitmap(MYTYPE *buffer, int len);
 
 int sprint_bitmap(char *outbuf, unsigned  short *buffer, int len);
 #else
