4a77695a71913cd346bf0930e2092f778a240304
Etienne Basset <etienne.basset@ensta.org>
2014-12-20 14:23:46 +0100

    update ipinfo
    Signed-off-by: Etienne Basset <etienne.basset@ensta.org>

--- ipinfo.c
+++ ipinfo.c
@@ -4,13 +4,49 @@
 #include "debug.h"
 #include "st_printf.h"
 
-void fprint_ipv4_info(FILE *out, struct subnet *subnet) {
 
+char ipv4_get_class(struct subnet *s) {
+	if ((s->ip >> 31) == 0)
+		return 'a';
+	if ((s->ip >> 30) == 2)
+		return 'b';
+	if ((s->ip >> 29) == 6)
+		return 'c';
+	if ((s->ip >> 28) == 14)
+		return 'd';
+	if ((s->ip >> 28) == 15)
+		return 'e';
+	return 0;
+}
+
+void fprint_multicast_ipv4_info(FILE *out, struct subnet *subnet) {
+
+}
+
+void fprintf_know_ipv4_ranges(FILE *out, struct subnet *subnet) {
+
+
+}
+
+void fprint_ipv4_info(FILE *out, struct subnet *subnet) {
+	char c;
+
+	c = ipv4_get_class(subnet);
+	fprintf(out, "Classfull info : class %c\n", c);
+	if (c == 'd') {
+		fprint_multicast_ipv4_info(out, subnet);
+		return;
+	}
+	if (c == 'e') {
+		fprintf(out, "Unassigned address range\n");
+		return;
+	}
 }
 
 void fprint_ipv6_info(FILE *out, struct subnet *subnet) {
 
 }
+
 void fprint_ip_info(FILE *out, struct subnet *subnet) {
 	fprintf(out, "IP version : %d\n", subnet->ip_ver);
 	st_fprintf(out, "Network Address : %N/%m\n", *subnet, *subnet);
@@ -21,5 +57,4 @@ void fprint_ip_info(FILE *out, struct subnet *subnet) {
 		fprint_ipv4_info(out, subnet);
 	if (subnet->ip_ver == IPV6_A)
 		fprint_ipv6_info(out, subnet);
-
 }
