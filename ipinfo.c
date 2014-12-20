d1b497337914879172058ecbc2c71bad96fb541b
Etienne Basset <etienne.basset@ensta.org>
2014-12-20 14:44:07 +0100

    add some know addresses
    Signed-off-by: Etienne Basset <etienne.basset@ensta.org>

--- ipinfo.c
+++ ipinfo.c
@@ -5,6 +5,30 @@
 #include "st_printf.h"
 
 
+struct subnet class_a = { .ip = 0, .mask = 1};
+struct subnet class_b = { .ip = (1 << 31), .mask = 2};
+struct subnet class_c = { .ip = (3 << 30), .mask = 3};
+struct subnet class_d = { .ip = (14 << 29), .mask = 4};
+struct subnet class_e = { .ip = (15 << 29), .mask = 4};
+
+struct subnet rfc1918_1;
+struct subnet rfc1918_2;
+struct subnet rfc1918_3;
+struct subnet ipv4_loopback;
+struct subnet rfc3927_ipv4_ll;
+struct subnet rfc6598_ipva4_sasr;
+
+void init_know_subnets() {
+	get_subnet_or_ip("10.0.0.0/8", &rfc1918_1);
+	get_subnet_or_ip("172.16.0.0/12", &rfc1918_2);
+	get_subnet_or_ip("192.168.0.0/16", &rfc1918_3);
+	get_subnet_or_ip("127.0.0.0/8", &ipv4_loopback);
+	get_subnet_or_ip("169.254.0.0/16", &rfc3927_ipv4_ll);
+	get_subnet_or_ip("100.64.0.0/10", &rfc6598_ipva4_sasr);
+
+
+}
+
 char ipv4_get_class(struct subnet *s) {
 	if ((s->ip >> 31) == 0)
 		return 'a';
