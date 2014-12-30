08bc697ff47e598e95daf820c675488ef41e38b9
etienne basset <etienne.basset@numericable.fr>
2014-12-30 14:33:00 +0100

    [MEDIUM] add sto2string function
    Signed-off-by: etienne basset <etienne.basset@numericable.fr>

--- st_object.c
+++ st_object.c
@@ -14,4 +14,38 @@
 #include "utils.h"
 #include "iptools.h"
 #include "debug.h"
+#include "st_object.h"
 
+
+int sto2string(char *s, const struct sto *o, size_t len, int comp_level) {
+	int l;
+	switch (o->type) {
+		case '[':
+		case 's':
+		case 'W':
+		case 'S':
+			return strxcpy(s, o->s_char, len);
+			break;
+		case 'I':
+			return addr2str(&o->s_addr, s, len, comp_level);
+			break;
+		case 'P':
+			return subnet2str(&o->s_subnet, s, len, comp_level);
+			break;
+		case 'd':
+		case 'M':
+			return snprintf(s, len, "%d", o->s_int);
+			break;
+		case 'l':
+			return snprintf(s, len, "%ld", o->s_long);
+			break;
+		case 'c':
+			s[0] = o->s_char[0];
+			s[1] = '\0';
+			return 1;
+			break;
+		default:
+			return -1;
+			break;
+	}
+}
