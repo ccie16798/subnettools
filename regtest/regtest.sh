f74c09438fcf45b16c5664f6392740c5e67c43f7
etienne basset <etienne.basset@numericable.fr>
2014-12-31 11:41:55 +0100

    [REGTEST] more scanf regtest
    Signed-off-by: etienne basset <etienne.basset@numericable.fr>

--- regtest/regtest.sh
+++ regtest/regtest.sh
@@ -35,10 +35,10 @@ reg_test_scanf() {
 	local output_file
 	local n
 
-	n=2
+	n=3
 	$PROG scanf "1.1.1.1 zob    1.1.1.2    name 25" " *%I (%S )?.*%I *(name) %d" > res/scanf1 
 	$PROG scanf "1.1.1.1   1.1.1.2    name 25" " *%I (%S )?.*%I *(name) %d" > res/scanf2 
-
+	$PROG scanf "1.1.1.1  1.1.1.2 2.2.2.2 toto   r" " *%I .*%S" > res/scanf3
 	for i in `seq 1 $n`; do
 		output_file=scanf$i
 		if [ ! -f ref/$output_file ]; then
