c38f547154779bd93f7872cc14e8922056f3e166
etienne basset <etienne.basset@numericable.fr>
2015-01-02 12:43:05 +0100

    [CONVERTER] move ciscoroute to st_sscanf
    Signed-off-by: etienne basset <etienne.basset@numericable.fr>

--- routetocsv.c
+++ routetocsv.c
@@ -34,14 +34,13 @@ void csvconverter_help(FILE *output);
 
 struct csvconverter csvconverters[] = {
 	{ "CiscoRouter", 	&cisco_route_to_csv, 	"ouput of 'show ip route' or 'sh ipv6 route' on Cisco IOS, IOS-XE" },
-	{ "CiscoRouterConf", &cisco_route_conf_to_csv, "full configuration or ipv6/ipv4 static routes"},
-	{ "CiscoFWConf", 	&cisco_fw_conf_to_csv, 	"ouput of 'show conf', IPv4 only"},
+	{ "CiscoRouterConf", 	&cisco_route_conf_to_csv,"full configuration or ipv6/ipv4 static routes"},
+	{ "CiscoFWConf", 	&cisco_fw_conf_to_csv, 	"ouput of 'show conf'"},
 	{ "IPSO",		&ipso_route_to_csv, 	"output of clish show route"  },
 	{ "GAIA",		&ipso_route_to_csv ,	"output of clish show route" },
 	{ "CiscoNexus",	&cisco_nexus_to_csv ,	"output of show ip route on Cisco Nexus NXOS" },
 	{ NULL, NULL }
 };
-
 
 void csvconverter_help(FILE *output) {
 	int i = 0;
@@ -109,32 +108,6 @@ int classfull_get_subnet(const char *string, struct subnet *subnet) {
 	sprintf(out, "%d.%d.%d.%d/%s", truc[0], truc[1], truc[2], truc[3], s);
 	return get_subnet_or_ip(out, subnet);
 }
-
-#define MOVE_TO_NEXT_TOKEN(__x) \
-	do { \
-		s = strtok(__x, " \n"); \
-		debug(PARSEROUTE, 9, "line %lu parsing token %d : %s \n", line, token, s); \
-		if (s==NULL) { \
-			debug(PARSEROUTE, 5, "line %lu is invalid, only %d tokens \n", line, token); \
-			badline = 1; \
-			break; \
-		}\
-		token++; \
-	} while (0); \
-if ( badline ) \
-	continue;
-#define COMMON_PARSER_HEADER \
-	char buffer[1024]; \
-	char *s; \
-	unsigned long line = 0; \
-	int badline = 0; \
-	struct subnet subnet; \
-	int token; \
-	int res; \
-	char *gw, *dev="", *comment=""; \
-	fprintf(output, "prefix;mask;device;GW;comment\n");
-
-
 
 int cisco_nexus_to_csv(char *name, FILE *f, FILE *output) {
 	char buffer[1024];
@@ -290,183 +263,126 @@ int ipso_route_to_csv(char *name, FILE *f, FILE *output) {
 /*
  * output of show ip route or show ipv6 route
  * cisco IOS, IOS-XE
- * cant be converted to generic_csv because too many special cases (wtf ? X.X.X.X is subnetted, ....)
  */
 int cisco_route_to_csv(char *name, FILE *f, FILE *output) {
-	int find_mask = -1;
-	int found_gw;
-	int num_mask;
-	char ip2[51];
+        char buffer[1024];
+        char *s;
+        unsigned long line = 0;
+        int badline = 0;
+        struct route route;
+        int res;
 	int ip_ver = -1;
-	struct ip_addr addr;
-	char *s2;
-	COMMON_PARSER_HEADER
+	int find_mask;
+	int is_subnetted = 0;
 
+	memset(&route, 0, sizeof(route));
+	fprintf(output, "prefix;mask;device;GW;comment\n");
 	while ((s = fgets_truncate_buffer(buffer, sizeof(buffer), f, &res))) {
 		line++;
-		dev="";
-		num_mask=0;
-		found_gw = 0;
-		token=1;
-		badline = 0;
-		if (res) {
-			debug(PARSEROUTE, 1, "File %s line %lu is longer than max size %d, discarding %d chars\n",
-					name, line, (int)sizeof(buffer), res);
-		}
-		debug(PARSEROUTE, 9, "parsing line %lu : %s\n", line, s);
-		if (strstr(s, "variably subnetted")!=NULL ) {
-			debug(PARSEROUTE, 5, "line %lu \'is variably subnetted\', ignoring\n", line);
+		debug(PARSEROUTE, 9, "line %lu buffer '%s'\n", line, buffer);
+		if (!strncmp(s, "Gateway ", 8)) {
+                        debug(PARSEROUTE, 5, "line %lu \'is gateway of last resort, skipping'\n", line);
+			continue;
+		} else if (strstr(s, "variably subnetted")) {
+                        debug(PARSEROUTE, 5, "line %lu \'is variably subnetted, skipping'\n", line);
 			continue;
 		} else if (strstr(s, "is subnetted")) {
 			debug(PARSEROUTE, 5, "line %lu \'is subnetted'\n", line);
-			MOVE_TO_NEXT_TOKEN(s);
-			res = get_subnet_or_ip(s, &subnet);
-			if ( res > 1000 ) {
-				debug(PARSEROUTE, 2, "line %lu bad IP %s \n", line, s);
+			find_mask = route.subnet.mask;
+			/*      194.51.71.0/32 is subnetted, 1 subnets */
+			res = st_sscanf(s, " *%I/%d is subnetted, %d subnets", &route.subnet.ip_addr, &find_mask, &is_subnetted);
+			if (res < 3) {
+				badline++;
+				debug(PARSEROUTE, 1, "line %lu invalid 'is subnetted' '%s'\n", line, s);
 				continue;
 			}
-			find_mask = subnet.mask;
 			continue;
-		} else if (strstr(s,"is directly connected")) {
-			char *previous_s;
-			debug(PARSEROUTE, 5, "line %lu connected route \n", line);
-			MOVE_TO_NEXT_TOKEN(s);
-			MOVE_TO_NEXT_TOKEN(NULL);
-			res = get_subnet_or_ip(s, &subnet);
-			if (res == IPV4_N) {
-				subnet2str(&subnet, ip2, sizeof(ip2), 2);
-				num_mask = subnet.mask;
-				gw = "0.0.0.0";
-			} else if (res == IPV6_N) {
-				/** in fact this should not happen */
-				gw = "::";
-			} else {
-				debug(PARSEROUTE, 2, "line %lu invalid connected IP %s \n", line, s);
+		} else if (strstr(s, "directly connected")) {
+			/* C       10.73.5.92/30 is directly connected, Vlan346 */
+			res = st_sscanf(s, ".*%P.*$%s", &route.subnet, route.device);
+			if (res < 2) {
+				badline++;
+				debug(PARSEROUTE, 1, "line %lu invalid connected route '%s'\n", line, s);
+				memset(&route, 0, sizeof(route));
 				continue;
 			}
-			if (ip_ver == -1) { /** checking the IP version is same as the first one we found **/
-				ip_ver = subnet.ip_ver % 10;
-			} else if (ip_ver != subnet.ip_ver) {
-				debug(PARSEROUTE, 1, "line %lu inconsistent IP version, not supported\n", line);
+			if (ip_ver == -1) {
+				ip_ver = route.subnet.ip_ver;
+			} else if (route.subnet.ip_ver != ip_ver) {
+				debug(PARSEROUTE, 1, "line %lu Invalid '%s', inconsistent IP version\n", line, s);
+				badline++;
 				continue;
 			}
-			while (1) { /* go to last token */
-				previous_s = s;
-				s = strtok(NULL, " \n");
-				if (s == NULL)
-					break;
-			}
-			dev = previous_s;
-			fprintf(output, "%s;%d;%s;%s;%s\n", ip2, num_mask, dev, gw, comment);
+			fprint_route(&route, output, 3);
+			memset(&route, 0, sizeof(route));
 			continue;
 		}
-
-		MOVE_TO_NEXT_TOKEN(s); /** type of route (part 1) **/
-		MOVE_TO_NEXT_TOKEN(NULL); /** type of route (part 2) or IP address**/
-
-		/** now, there could be an IP adress **/
-		res = get_subnet_or_ip(s, &subnet);
-		if (res > 1000) {
-			debug(PARSEROUTE, 6, "line %lu %s is not IP, probably route type\n", line, s);
-			MOVE_TO_NEXT_TOKEN(NULL); /** IP address**/
-			res = get_subnet_or_ip(s, &subnet);
-		}
-		if (res > 1000) {
-			debug(PARSEROUTE, 5, "line %lu %s is not IP, probably route type\n", line, s);
-			continue;
-		} else if (res == IPV4_A) { /* it is a line after x.X.X.X/mask is subnetted; so it doesnt have a mask */
-			strcpy(ip2, s);;
-			num_mask = find_mask;
-			debug(PARSEROUTE, 5, "line %lu found IP %s without mask\n", line, s);
-			s = strtok(NULL, "\n, ");
-			debug(PARSEROUTE, 9, "line %lu parsing token %d : %s \n", line, token++, s);
-		} else if (res == IPV4_N) {
-			subnet2str(&subnet, ip2, sizeof(ip2), 2);
-			num_mask = subnet.mask;
-			debug(PARSEROUTE, 5, "line %lu found IP/mask %s\n", line, s);
-			s = strtok(NULL, "\n, ");
-			debug(PARSEROUTE, 9, "line %lu parsing token %d : %s \n", line, token++, s);
-		} else if (res == IPV6_N) {
-			subnet2str(&subnet, ip2, sizeof(ip2), 2);
-			num_mask = subnet.mask;
-			debug(PARSEROUTE, 5, "line %lu found IPv6/mask %s\n", line, s);
-			/** in show ipv6 route, the NH is printed on the next line so get a new line **/
-			s = fgets(buffer, sizeof(buffer), f);
-			line++;
-			token = 1;
-			if (s == NULL) {
-				debug(PARSEROUTE, 3, "no line %lu but there should be one, cannot get a next hop\n", line);
+		if (is_subnetted) {
+			res = st_sscanf(s, ".*%I.*(via) %I.*$%S", &route.subnet.ip_addr, &route.gw, route.device);
+			if (res < 2) {
+				badline++;
+				debug(PARSEROUTE, 1, "line %lu invalid 'is subnetted' '%s'\n", line, s);
+				is_subnetted = 0;
 				continue;
 			}
-			s = strtok(s, "\n, ");
-			debug(PARSEROUTE, 9, "line %lu parsing token %d : %s \n", line, token++, s);
-		} else if (res == IPV6_A) {
-			debug(PARSEROUTE, 5, "line %lu found IPv6 %s without a mask, BUG\n", line, s);
+			if (res == 2) {
+				debug(PARSEROUTE, 1, "line %lu no device found\n", line);
+				route.device[0] = '\0';	
+			}
+			if (ip_ver == -1) {
+				ip_ver = route.subnet.ip_ver;
+			} else if (route.subnet.ip_ver != ip_ver) {
+				debug(PARSEROUTE, 1, "line %lu Invalid '%s', inconsistent IP version\n", line, s);
+				badline++;
+				continue;
+			}
+			route.subnet.mask = find_mask;
+			is_subnetted--;
+			fprint_route(&route, output, 3);
+			memset(&route, 0, sizeof(route));
+			continue;
+		}
+		res = st_sscanf(s, ".*%P.*(via) %I.*$%s", &route.subnet, &route.gw, route.device);
+		if (res == 0) {
+			badline++;
+			debug(PARSEROUTE, 1, "line %lu Invalid line '%s'\n", line, s);
+			memset(&route, 0, sizeof(route));
 			continue;
 		}
 		if (ip_ver == -1) {
-			ip_ver = subnet.ip_ver;
-		} else if (ip_ver != subnet.ip_ver) {
-			debug(PARSEROUTE, 1, "line %lu inconsistent IP version, not supported\n", line);
+			ip_ver = route.subnet.ip_ver;
+		} else if (route.subnet.ip_ver != ip_ver) {
+			debug(PARSEROUTE, 1, "line %lu Invalid '%s', inconsistent IP version\n", line, s);
+			badline++;
 			continue;
 		}
-
-		while (s) { /** the gateway is after the 'via' keyword */
-			if (s == NULL)
-				break;
-			if (!strcasecmp(s, "via")) {
-				found_gw = 1;
+		if (ip_ver == IPV6_A) {
+			s = fgets_truncate_buffer(buffer, sizeof(buffer), f, &res);
+			line++;
+			if (s == NULL) {
+				debug(PARSEROUTE, 1, "line %lu Invalid line, no next hop found\n", line);
 				break;
 			}
-			s = strtok(NULL, "\n, ");
-			token++;
-		}
-		if (found_gw == 0) {
-			debug(PARSEROUTE, 2, "line %lu no gw found for route\n", line);
+			res = st_sscanf(s, " *(via) (%I)?.*%[^, \n]", &route.gw, route.device);
+			if (res == 0) {
+				debug(PARSEROUTE, 1, "line %lu Invalid line '%s', no next-hop\n", line, s);
 				continue;
 			}
-		s = strtok(NULL, "\n, ");
-		debug(PARSEROUTE, 9, "line %lu parsing token %d : %s \n", line, token++, s);
-		if (s == NULL) {
-			debug(PARSEROUTE, 2, "line %lu no GW after via field\n", line);
-			continue;
-		}
-
-		res = get_single_ip(s, &addr, 41);
-		if (ip_ver == IPV4_A && res == IPV4_A) {
-			debug(PARSEROUTE, 5, "line %lu gw %s \n", line, s);
-			gw = s;
-		} else if (ip_ver == IPV6_A && res == IPV6_A) {
-			debug(PARSEROUTE, 5, "line %lu gw6 %s \n", line, s);
-			gw = s;
-		} else if (ip_ver == IPV6_A && res  > 1000) { /** for IPv6, there can be no NH or in case of MPBGP a NH in global table **/
-			debug(PARSEROUTE, 5, "line %lu gw6 %s \n", line, s);
-			dev = s;
-			gw = "";
-			fprintf(output, "%s;%d;%s;%s;%s\n", ip2, num_mask, dev, gw, comment);
-			continue;
 		} else {
-			debug(PARSEROUTE, 2, "line %lu gw %s invalid IP\n", line, s);
-			gw = "";
+			if (ip_ver == -1)
+				ip_ver = IPV6_A;
+			if (res < 2) {
+				debug(PARSEROUTE, 1, "line %lu Invalid line '%s', no next-hop\n", line, s);
+				continue;
+			}
 		}
-		/* trying to find the device, if any
-		   device will be the last token */
-		s = strtok(NULL, "\n, ");
-		s2 = NULL;
-		while (s) {
-			s2 = s;
-			s = strtok(NULL, "\n, ");
-			debug(PARSEROUTE, 9, "line %lu parsing token %d : %s \n", line, token++, s);
-		}
-		if (s2)
-			dev = s2;
-		else
-			dev = "";
-		fprintf(output, "%s;%d;%s;%s;%s\n", ip2, num_mask, dev, gw, comment);
-
-	} // while
-	return 0;
+		fprint_route(&route, output, 3);
+		memset(&route, 0, sizeof(route));
+	}
+	return 1;
 }
+
+
 
 int cisco_fw_conf_to_csv(char *name, FILE *f, FILE *output) {
 	char buffer[1024];
