647703a02ff12e4d921c232cca36a381d04e235b
etienne basset <etienne.basset@numericable.fr>
2015-01-05 17:22:51 +0100

    [TEST] update test for hex
    Signed-off-by: etienne basset <etienne.basset@numericable.fr>

--- t.c
+++ t.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "iptools.h"
+#include "utils.h"
 /* staging testing file 
  * not used in Makefile */
 struct options {
@@ -18,66 +19,15 @@ struct options {
 };
 
 
-#define sprint_hex(__type) \
-inline int  sprint_hex##__type(char *s, unsigned __type a) { \
-        char c[32]; \
-        int j, i = 0; \
-\
-        do { \
-		if (a % 16 < 10) \
-                	c[i] = '0' + a % 16; \
-		else \
-			c[i] = 'a' + (a - 10) % 16; \
-                a /= 16; \
-                i++; \
-        } while (a); \
-        for (j = 0; j < i;j++) \
-                s[j] = c[i - j - 1]; \
-        return i; \
-}
-
-#define sprint_unsigned(__type) \
-inline int  sprint_u##__type(char *s, unsigned __type a) { \
-	char c[32]; \
-	int j, i = 0; \
-\
-	do { \
-		c[i] = '0' + a % 10; \
-		a /= 10; \
-		i++; \
-	} while (a); \
-	for (j = 0; j < i;j++) \
-		s[j] = c[i - j - 1]; \
-	return i; \
-}
-
-#define  sprint_signed(__type) \
-inline int sprint_##__type (char *s, __type b) { \
-	char c[32]; \
-	int j, i = 0; \
-	unsigned __type a = b; \
-	if ( a >  ((unsigned __type)-1) / 2) { \
-		s[0] = '-'; \
-		a = ((unsigned __type)-1) - a; \
-		a++; \
-		s++; \
-	} \
-\
-	do { \
-		c[i] = '0' + a % 10; \
-		a /= 10; \
-		i++; \
-	} while (a); \
-	for (j = 0; j < i; j++) \
-		s[j ] = c[i -j - 1]; \
-	return i; \
-}
 sprint_signed(short)
 sprint_hex(short)
 sprint_signed(int)
 sprint_unsigned(short)
 sprint_unsigned(int)
 sprint_unsigned(long)
+
+snprint_hex(short)
+snprint_signed(int)
 
 #define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
 
@@ -86,10 +36,17 @@ int main(int argc, char **argv) {
 	char buff[32];
 	struct subnet s;
 	short a;
+	int b = -1234;
+	int res;
+
 	memset(buff, 0, 28);
-	 sscanf(argv[1], "%x", &a);
-	sprint_hexshort(buff, a);
-	printf("%s %hd\n", buff, a);
+	sscanf(argv[1], "%x", &a);
+	res = snprint_hexshort(buff, a, 2);
+	buff[res] = '\0';
+	printf("%s\n", buff);
+	res = snprint_int(buff, b, 3);
+	buff[res] = '\0';
+	printf("%s\n", buff);
 
 //	printf("%d %d %d \n", offsetof(struct options, delim),  offsetof(struct options, delim2),  offsetof(struct options, delim4));
 	printf("%d %d\n", sizeof(long), sizeof(unsigned long long));
