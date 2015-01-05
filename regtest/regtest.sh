f7b544bcc6e1ebd6aa11a657c74333a3a119299e
etienne basset <etienne.basset@numericable.fr>
2015-01-05 17:59:38 +0100

    [REGTEST] add test for HEX scanning
    Signed-off-by: etienne basset <etienne.basset@numericable.fr>

--- regtest/regtest.sh
+++ regtest/regtest.sh
@@ -39,7 +39,7 @@ reg_test_scanf() {
 	local output_file
 	local n
 
-	n=19
+	n=20
 	$PROG scanf "1.1.1.1 zob    1.1.1.2    name 25" " *%I (%S )?.*%I *(name) %d" > res/scanf1 
 	$PROG scanf "1.1.1.1   1.1.1.2    name 25" " *%I (%S )?.*%I *(name) %d" > res/scanf2 
 	$PROG scanf "1.1.1.1  1.1.1.2 2.2.2.2 toto   r" " *%I .*%S" > res/scanf3
@@ -58,14 +58,15 @@ reg_test_scanf() {
 	$PROG scanf "ab]ca1.1.1.1" "[]abc]*%I" > res/scanf14
 	# exercise '%Q'
 	$PROG scanf " 1.1/16 1.1.1.1 toto" " *%Q.*$%S" > res/scanf15
-	#exercise End on char range (the range matches 'f')
+	#exercise End on char range (the range below matches 'f')
 	$PROG scanf "abcdef1.1.1.1"   ".*[f-g]%I"  > res/scanf16
 	$PROG scanf "abcdef1.1.1.1"   ".*[^a-e]%I" > res/scanf17
-	#exercise short and signed, unsigned
+	# test short and signed, unsigned
 	$PROG scanf  "  65000  3000000000  3000000000" ".*%hd.*%d.*%u" > res/scanf18
-	# exercise max field length
+	# test max field length
 	$PROG scanf  "1234567890  abcdabcdabcd 1234567890"  "%9s[90 ]*%5[abcd](abcdabcd).*%9S" > res/scanf19
-
+	# test HEX scanning
+	$PROG scanf "0xfffeabc 200 0xfff1fffe" "%x *%hx .*$%lx" > res/scanf20
 
 	for i in `seq 1 $n`; do
 		output_file=scanf$i
