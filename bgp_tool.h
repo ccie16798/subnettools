00e935698354be9e10d43770a479bd8f6b2c30e9
Etienne Basset <etienne.basset@ensta.org>
2015-08-02 10:09:16 +0200

    [BGP] fprint_route should appear in .h
    Signed-off-by: Etienne Basset <etienne.basset@ensta.org>

--- bgp_tool.h
+++ bgp_tool.h
@@ -1,7 +1,7 @@
 #ifndef BGPTOOL_H
 #define BGPTOOL_H
 
-
+void fprint_bgp_route(FILE *, struct bgp_route *r);
 int load_bgpcsv(char  *name, struct bgp_file *sf, struct st_options *nof);
 int compare_bgp_file(const struct bgp_file *sf1, const struct bgp_file *sf2, struct st_options *o);
 
