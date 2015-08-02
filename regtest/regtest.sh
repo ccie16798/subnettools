b67f53463a4b4e2b6a1b74f63ea9864a738f567f
Etienne Basset <etienne.basset@ensta.org>
2015-08-02 18:51:34 +0200

    [REGTEST] now BGP loading is fixed, add regtest
    Signed-off-by: Etienne Basset <etienne.basset@ensta.org>

--- regtest/regtest.sh
+++ regtest/regtest.sh
@@ -177,6 +177,10 @@ reg_test convert CiscoNexus iproutecisconexus_ECMP
 reg_test convert palo iproute_palo
 reg_test convert ciscobgp ciscobgp
 reg_test bgpcmp  bgp1 bgp2
+reg_test bgpsortby med bgp1
+reg_test bgpsortby prefix bgp1
+reg_test bgpsortby localpref bgp1
+reg_test bgpsortby gw bgp1
 
 reg_test grep mergeipv6 2001:db8::
 #ipinfo
