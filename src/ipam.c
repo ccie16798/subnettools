8cb7a273f86f3d83a76edf746dd9986c608d9641
Etienne Basset <etienne.basset@ensta.org>
2015-08-22 14:47:58 +0200

    [IPAM] continue
    Signed-off-by: Etienne Basset <etienne.basset@ensta.org>

--- src/ipam.c
+++ src/ipam.c
@@ -2,4 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "debug.h"
 #include "ipam.h"
+#include "generic_csv.h"
+
+int alloc_ipam_file(struct ipam_file *sf, unsigned long n) {
+	if (n > SIZE_T_MAX / sizeof(struct ipam)) { /* being paranoid */
+		fprintf(stderr, "error: too much memory requested for struct ipam\n");
+		return -1;
+	}
+	sf->routes = malloc(sizeof(struct ipam) * n);
+	if (sf->routes == NULL) {
+		fprintf(stderr, "Cannot alloc  memory (%lu Kbytes) for sf->ipam\n",
+				n * sizeof(struct ipam));
+		sf->nr = sf->max_nr = 0;
+		return -1;
+	}
+	debug(MEMORY, 3, "Allocated %lu bytes for subnet_file\n",  sizeof(struct ipam) * n);
+	sf->nr = 0;
+	sf->max_nr = n;
+	return 0;
+}
+
+static int ipam_prefix_handle(char *s, void *data, struct csv_state *state) {
+	struct ipam_file *sf = data;
+	int res;
+	struct subnet subnet;
+
+	res = get_subnet_or_ip(s, &subnet);
+	if (res < 0) {
+		debug(LOAD_CSV, 2, "invalid IP %s line %lu\n", s, state->line);
+		return CSV_INVALID_FIELD_BREAK;
+	}
+	copy_subnet(&sf->routes[sf->nr].subnet,  &subnet);
+	return CSV_VALID_FIELD;
+}
+
+static int ipam_mask_handle(char *s, void *data, struct csv_state *state) {
+	struct ipam_file *sf = data;
+	int mask = string2mask(s, 21);
+
+	if (mask < 0) {
+		debug(LOAD_CSV, 2, "invalid mask %s line %lu\n", s, state->line);
+		return CSV_INVALID_FIELD_BREAK;
+	}
+	sf->routes[sf->nr].subnet.mask = mask;
+	return CSV_VALID_FIELD;
+}
+
--- src/ipam.h
+++ src/ipam.h
@@ -3,9 +3,13 @@
 
 #include "iptools.h"
 
+#ifndef SIZE_T_MAX
+#define SIZE_T_MAX ((size_t)0 - 1)
+#endif
+
 struct  ipam_ea {
 	char *name;
-	int type:
+	int type;
 	void *value;
 };
 
@@ -14,6 +18,16 @@ struct ipam {
 	int ea_nr; /* number of Extensible Attributes */
 	struct ipam_ea *ea;
 };
+
+
+struct ipam_file {
+	struct ipam *routes;
+	unsigned long nr;
+	unsigned long max_nr; /* the number of routes that has been malloced */
+
+};
+
+int alloc_ipam_file(struct ipam_file *sf, unsigned long n);
 
 
 #else
