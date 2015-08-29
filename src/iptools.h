7036affaf77b25d9baed4841dba42f4f7d90af37
Etienne Basset <etienne.basset@ensta.org>
2015-08-29 21:56:24 +0200

    [TOOLS] add prototypes
    Signed-off-by: Etienne Basset <etienne.basset@ensta.org>

--- src/iptools.h
+++ src/iptools.h
@@ -97,6 +97,10 @@ void copy_ipaddr(struct ip_addr *a, const struct ip_addr *b);
 void copy_subnet(struct subnet *a, const struct subnet *b);
 void copy_route(struct route *a, const struct route *b);
 void zero_route(struct route *a);
+void zero_route_ea(struct route *a);
+int alloc_route_ea(struct route *r, int n);
+void free_route(struct route *r);
+
 void zero_ipaddr(struct ip_addr *a);
 int is_equal_ipv6(ipv6 ip1, ipv6 ip2);
 int is_equal_ip(struct ip_addr *ip1, struct ip_addr *p2);
