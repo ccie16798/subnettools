d457cca424600619f160d6a29c00ffdc2ee6381b
Etienne Basset <etienne.basset@ensta.org>
2015-10-26 15:24:11 +0100

    [MEMORY] use __func_ instead of __FUNCTION__
    Signed-off-by: Etienne Basset <etienne.basset@ensta.org>

--- src/st_memory.h
+++ src/st_memory.h
@@ -15,23 +15,23 @@ void *__st_malloc_nodebug(unsigned long n, const char *s,
 void *__st_malloc(unsigned long n, const char *s,
 		const char *file, const char *func, int line);
 #define st_malloc(__n, __s) \
-	__st_malloc(__n, __s, __FILE__, __FUNCTION__, __LINE__)
+	__st_malloc(__n, __s, __FILE__, __func__, __LINE__)
 
 #define st_malloc_nodebug(__n, __s) \
-	__st_malloc_nodebug(__n, __s, __FILE__, __FUNCTION__, __LINE__)
+	__st_malloc_nodebug(__n, __s, __FILE__, __func__, __LINE__)
 void *__st_realloc_nodebug(void *ptr, unsigned long new, unsigned long old, const char *s,
 		const char *file, const char *func, int line);
 void *__st_realloc(void *ptr, unsigned long newn, unsigned long oldn, const char *s,
 		const char *file, const char *func, int line);
 #define st_realloc_nodebug(__ptr, __new, __old, __s) \
-	__st_realloc_nodebug(__ptr, __new, __old, __s, __FILE__, __FUNCTION__, __LINE__)
+	__st_realloc_nodebug(__ptr, __new, __old, __s, __FILE__, __func__, __LINE__)
 
 #define st_realloc(__ptr, __new, __old, __s) \
-	__st_realloc(__ptr, __new, __old, __s, __FILE__, __FUNCTION__, __LINE__)
+	__st_realloc(__ptr, __new, __old, __s, __FILE__, __func__, __LINE__)
 char *__st_strdup(const char *s,
 		const char *file, const char *func, int line);
 #define st_strdup(__s) \
-		__st_strdup(__s, __FILE__, __FUNCTION__, __LINE__)
+		__st_strdup(__s, __FILE__, __func__, __LINE__)
 void st_free_string(char *s);
 void st_free(void *ptr, unsigned long len);
 
