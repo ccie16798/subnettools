83e3ba6afa7c120eadd1a1bdbe93f1058c955b39
Etienne Basset <etienne.basset@ensta.org>
2015-10-26 15:32:58 +0100

    [STOBJECT] indentation fixes
    Signed-off-by: Etienne Basset <etienne.basset@ensta.org>

--- src/st_object.c
+++ src/st_object.c
@@ -16,7 +16,6 @@
 #include "debug.h"
 #include "st_object.h"
 
-
 snprint_signed(short)
 snprint_signed(int)
 snprint_signed(long)
@@ -39,68 +38,68 @@ int sto2string(char *s, const struct sto *o, size_t len, int comp_level)
 	int res;
 
 	switch (o->type) {
-		case '[':
-		case 's':
-		case 'W':
-		case 'S':
-			return strxcpy(s, o->s_char, len);
-			break;
-		case 'I':
-			return addr2str(&o->s_addr, s, len, comp_level);
-			break;
-		case 'Q':
-		case 'P':
-			subnet2str(&o->s_subnet, buffer, sizeof(buffer), comp_level);
-			return snprintf(s, len, "%s/%d", buffer, (int)o->s_subnet.mask);
-			break;
-		case 'x':
-			if (o->conversion == 'l')
-				res = snprint_hexlong(s, o->s_ulong, len - 1);
-			else if (o->conversion == 'h')
-				res = snprint_hexshort(s, o->s_ushort, len - 1);
-			else
-				res = snprint_hexint(s, o->s_uint, len - 1);
-			s[res] = '\0';
-			return res;
-			break;
-		case 'u':
-			if (o->conversion == 'l')
-				res = snprint_ulong(s, o->s_ulong, len - 1);
-			else if (o->conversion == 'h')
-				res = snprint_ushort(s, o->s_ushort, len - 1);
-			else
-				res = snprint_uint(s, o->s_uint, len - 1);
-			s[res] = '\0';
-			return res;
-			break;
-		case 'd':
-			if (o->conversion == 'l')
-				res = snprint_long(s, o->s_long, len - 1);
-			else if (o->conversion == 'h')
-				res = snprint_short(s, o->s_short, len - 1);
-			else
-				res = snprint_int(s, o->s_int, len - 1);
-			s[res] = '\0';
-			return res;
-			break;
-		case 'M':
-			res = snprint_int(s, o->s_int, len - 1);
-			s[res] = '\0';
-			return res;
-			break;
-		case 'l':
+	case '[':
+	case 's':
+	case 'W':
+	case 'S':
+		return strxcpy(s, o->s_char, len);
+		break;
+	case 'I':
+		return addr2str(&o->s_addr, s, len, comp_level);
+		break;
+	case 'Q':
+	case 'P':
+		subnet2str(&o->s_subnet, buffer, sizeof(buffer), comp_level);
+		return snprintf(s, len, "%s/%d", buffer, (int)o->s_subnet.mask);
+		break;
+	case 'x':
+		if (o->conversion == 'l')
+			res = snprint_hexlong(s, o->s_ulong, len - 1);
+		else if (o->conversion == 'h')
+			res = snprint_hexshort(s, o->s_ushort, len - 1);
+		else
+			res = snprint_hexint(s, o->s_uint, len - 1);
+		s[res] = '\0';
+		return res;
+		break;
+	case 'u':
+		if (o->conversion == 'l')
+			res = snprint_ulong(s, o->s_ulong, len - 1);
+		else if (o->conversion == 'h')
+			res = snprint_ushort(s, o->s_ushort, len - 1);
+		else
+			res = snprint_uint(s, o->s_uint, len - 1);
+		s[res] = '\0';
+		return res;
+		break;
+	case 'd':
+		if (o->conversion == 'l')
 			res = snprint_long(s, o->s_long, len - 1);
-			s[res] = '\0';
-			return res;
-			break;
-		case 'c':
-			s[0] = o->s_char[0];
-			s[1] = '\0';
-			return 1;
-			break;
-		default:
-			s[0] = '\0';
-			break;
+		else if (o->conversion == 'h')
+			res = snprint_short(s, o->s_short, len - 1);
+		else
+			res = snprint_int(s, o->s_int, len - 1);
+		s[res] = '\0';
+		return res;
+		break;
+	case 'M':
+		res = snprint_int(s, o->s_int, len - 1);
+		s[res] = '\0';
+		return res;
+		break;
+	case 'l':
+		res = snprint_long(s, o->s_long, len - 1);
+		s[res] = '\0';
+		return res;
+		break;
+	case 'c':
+		s[0] = o->s_char[0];
+		s[1] = '\0';
+		return 1;
+		break;
+	default:
+		s[0] = '\0';
+		break;
 	}
 	return -1;
 }
--- src/st_object.h
+++ src/st_object.h
@@ -3,18 +3,18 @@
 #include "iptools.h"
 
 /* subnet tools object */
-struct sto { 
+struct sto {
 	char type;
 	char conversion; /* like 'l' for long int */
 	union {
-		struct subnet 	s_subnet;
-		struct ip_addr 	s_addr;
-		int 		s_int;
+		struct subnet	s_subnet;
+		struct ip_addr	s_addr;
+		int		s_int;
 		short		s_short;
 		unsigned short	s_ushort;
-		unsigned int 	s_uint;
-		long 		s_long;
-		unsigned long 	s_ulong;
+		unsigned int	s_uint;
+		long		s_long;
+		unsigned long	s_ulong;
 		char		s_char[64];
 	};
 };
@@ -23,7 +23,8 @@ int sto_is_string(struct sto *o);
 
 int sto2string(char *s, const struct sto *o, size_t len, int comp_level);
 
-#define consume_valist_from_object(__o, __n, __ap) do { \
+#define consume_valist_from_object(__o, __n, __ap) \
+do { \
 	int __i; \
 	void *ptr; \
 	\
@@ -31,60 +32,60 @@ int sto2string(char *s, const struct sto *o, size_t len, int comp_level);
 		debug(SCANF, 8, "restoring '%c' to va_list\n", __o[__i].type); \
 		ptr = va_arg(__ap, void *); \
 		switch (__o[__i].type) { \
-			case '[': \
-			case 's': \
-			case 'W': \
-			case 'S': \
-				  strcpy((char *)ptr, __o[__i].s_char); \
-				break; \
-			case 'I': \
-				  copy_ipaddr((struct ip_addr *)ptr, &__o[__i].s_addr); \
-				break; \
-			case 'P': \
-			case 'Q': \
-				  copy_subnet((struct subnet *)ptr, &__o[__i].s_subnet); \
+		case '[': \
+		case 's': \
+		case 'W': \
+		case 'S': \
+			strcpy((char *)ptr, __o[__i].s_char); \
 			break; \
-			case 'x': \
-				if (__o[__i].conversion  == 'l') \
-					*((unsigned long *)ptr) = __o[__i].s_ulong; \
-				else if (__o[__i].conversion  == 'h') \
-					*((unsigned short *)ptr) = __o[__i].s_ushort; \
-				else \
-					*((unsigned int *)ptr) = __o[__i].s_uint; \
-				break; \
-			case 'u': \
-				if (__o[__i].conversion  == 'l') \
-					*((unsigned long *)ptr) = __o[__i].s_ulong; \
-				else if (__o[__i].conversion  == 'h') \
-					*((unsigned short *)ptr) = __o[__i].s_ushort; \
-				else \
-					*((unsigned int *)ptr) = __o[__i].s_uint; \
-				break; \
-			case 'd': \
-				if (__o[__i].conversion  == 'l') \
-					*((long *)ptr) = __o[__i].s_long; \
-				else if (__o[__i].conversion  == 'h') \
-					*((short *)ptr) = __o[__i].s_short; \
-				else \
-					*((int *)ptr) = __o[__i].s_int; \
-				break; \
-			case 'M': \
-				  *((int *)ptr) = __o[__i].s_int; \
-				break; \
-			case 'l': \
-				  *((long *)ptr) = __o[__i].s_long; \
-				break; \
-			case 'c': \
-				  *((char *)ptr) = __o[__i].s_char[0]; \
-				break; \
-			case '\0': \
-				break; \
-			default: \
-				 debug(SCANF, 1, "Unknown object type '%c'\n", __o[__i].type); \
+		case 'I': \
+			copy_ipaddr((struct ip_addr *)ptr, &__o[__i].s_addr); \
+			break; \
+		case 'P': \
+		case 'Q': \
+			copy_subnet((struct subnet *)ptr, &__o[__i].s_subnet); \
+			break; \
+		case 'x': \
+			if (__o[__i].conversion  == 'l') \
+				*((unsigned long *)ptr) = __o[__i].s_ulong; \
+			else if (__o[__i].conversion  == 'h') \
+				*((unsigned short *)ptr) = __o[__i].s_ushort; \
+			else \
+				*((unsigned int *)ptr) = __o[__i].s_uint; \
+			break; \
+		case 'u': \
+			if (__o[__i].conversion  == 'l') \
+				*((unsigned long *)ptr) = __o[__i].s_ulong; \
+			else if (__o[__i].conversion  == 'h') \
+				*((unsigned short *)ptr) = __o[__i].s_ushort; \
+			else \
+				*((unsigned int *)ptr) = __o[__i].s_uint; \
+			break; \
+		case 'd': \
+			if (__o[__i].conversion  == 'l') \
+				*((long *)ptr) = __o[__i].s_long; \
+			else if (__o[__i].conversion  == 'h') \
+				*((short *)ptr) = __o[__i].s_short; \
+			else \
+				*((int *)ptr) = __o[__i].s_int; \
+			break; \
+		case 'M': \
+			*((int *)ptr) = __o[__i].s_int; \
+			break; \
+		case 'l': \
+			*((long *)ptr) = __o[__i].s_long; \
+			break; \
+		case 'c': \
+			*((char *)ptr) = __o[__i].s_char[0]; \
+			break; \
+		case '\0': \
+			break; \
+		default: \
+			debug(SCANF, 1, "Unknown object type '%c'\n", __o[__i].type); \
+			break; \
 		} \
 	} \
-	\
-} while (0);
+} while (0)
 
 #else
 #endif
