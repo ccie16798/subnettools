b65698395b227b9a49538a5aef72cb9a0eac25cb
Etienne Basset <etienne.basset@ensta.org>
2015-11-06 23:49:32 +0100

    [REGTES] add 2 regtest for special pattern matching
    Signed-off-by: Etienne Basset <etienne.basset@ensta.org>

--- regtest/regtest.sh
+++ regtest/regtest.sh
@@ -40,7 +40,7 @@ reg_test_scanf() {
 	local output_file
 	local n
 
-	n=38
+	n=40
 	$PROG scanf "1.1.1.1 zob    1.1.1.2    name 25" " *%I (%S )?.*%I *(name) %d" > res/scanf1 
 	$PROG scanf "1.1.1.1   1.1.1.2    name 25" " *%I (%S )?.*%I *(name) %d" > res/scanf2 
 	$PROG scanf "1.1.1.1  1.1.1.2 2.2.2.2 toto   r" " *%I .*%S" > res/scanf3
@@ -97,7 +97,9 @@ reg_test_scanf() {
 	$PROG scanf "[[[[[[[aaa" "\[{2,}%s" > res/scanf37
 	# end on long (with a stupid field length)
 	$PROG scanf "azazz112222" ".*%10ld" > res/scanf38
-	
+	# special chars
+	$PROG scanf "ab[e" "ab\[*%c" > res/scanf39
+	$PROG scanf "ab[e" "ab\[%c" > res/scanf40
 
 	for i in `seq 1 $n`; do
 		output_file=scanf$i
