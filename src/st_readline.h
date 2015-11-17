357d327c7bf32df3f5cde8c7a76d082911634c61
Etienne Basset <etienne.basset@ensta.org>
2015-11-17 21:55:36 +0100

    [READLINE] add prototype for st_gets_truncates
    Signed-off-by: Etienne Basset <etienne.basset@ensta.org>

--- src/st_readline.h
+++ src/st_readline.h
@@ -28,6 +28,7 @@ void st_close(struct st_file *f);
 
 /* st_getline_truncate: read one line from a file, truncate if line too long
  * if strlen(line) > size, chars are DISCARDED until a NEWLINE is found
+ * returns a pointer to an internal buffer
  * @f         : struct file
  * @size      : read at most size char on each line
  * @read      : pointer to the number of char read (equals to strlen(s) + 1)
@@ -37,5 +38,20 @@ void st_close(struct st_file *f);
  *	NULL on error or EOF
  */
 char *st_getline_truncate(struct st_file *f, size_t size, int *read, int *discarded);
+
+/* st_gets_truncate: read one line from a file, truncate if line too long
+ * if strlen(line) > size, chars are DISCARDED until a NEWLINE is found
+ * store line in a user provided buffer
+ * @f         : struct file
+ * @buffer    : where to store the data
+ * @size      : read at most size char on each line (sizeof(buffer))
+ * @read      : pointer to the number of char read (equals to strlen(s) + 1)
+ * @discarded : number of discarded chars (not always precise)
+ * returns:
+ *	pointer to the line
+ *	NULL on error or EOF
+ */
+char *st_gets_truncate(struct st_file *f, char *buffer, size_t size,
+		int *read, int *discarded)
 #else
 #endif
