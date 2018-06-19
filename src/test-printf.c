f49a1b5f686c2e2ea706bc517f04e54db9f23403
Etienne Basset <etienne.basset@ensta.org>
2018-06-19 21:42:42 +0200

    [TESTPRINTF] style issues
    Signed-off-by: Etienne Basset <etienne.basset@ensta.org>

--- src/test-printf.c
+++ src/test-printf.c
@@ -18,19 +18,19 @@ snprint_unsigned(long)
 
 int main(int argc, char **argv)
 {
-	int 		a1 = 1000;
-	unsigned 	a2 = 4000000000UL;
-	int 		a3 = 4000000000UL;
-	int 		a4 = -123;
-	unsigned short b = 65535;
+	int		a1 = 1000;
+	unsigned int	a2 = 4000000000UL;
+	int		a3 = 4000000000UL;
+	int		a4 = -123;
+	unsigned short	b  = 65535;
 	/* long result will vary */
 	long		l1 = 9223372036854775805;
 	long		l2 = 300000000;
 	long		l3 = -1;
 	unsigned long   l4 = -1;
 	unsigned long   l5 = 200000000;
-	short 		sh1 = 32767;
-	short 		sh2 = 32768;
+	short		sh1 = 32767;
+	short		sh2 = 32768;
 	unsigned short  sh3 = 0xffff;
 	struct subnet s1, s2;
 	char *string = "STRING1";
@@ -66,19 +66,21 @@ int main(int argc, char **argv)
 	printf("TEST INT2   : '%06d' '%20u' '%-20d'\n", a1, a2, a3);
 	st_printf("TEST LONG   : %ld %ld %ld %lu %lu\n", l1, l2, l3, l4, l5);
 	printf("TEST LONG   : %ld %ld %ld %lu %lu\n", l1, l2, l3, l4, l5);
-	st_printf("TEST LONG2  : '%013ld' '%13ld' '%-4ld' '%lu' '%12lu'\n", l1, l2, l3, l4, l5);
-	printf("TEST LONG2  : '%013ld' '%13ld' '%-4ld' '%lu' '%12lu'\n", l1, l2, l3, l4, l5);
+	st_printf("TEST LONG2  : '%013ld' '%13ld' '%-4ld' '%lu' '%12lu'\n",
+			l1, l2, l3, l4, l5);
+	printf("TEST LONG2  : '%013ld' '%13ld' '%-4ld' '%lu' '%12lu'\n",
+			l1, l2, l3, l4, l5);
 	st_printf("TEST HEX1 : %x %hx %lx\n", a2, b, l5);
 	printf("TEST HEX1 : %x %hx %lx\n", a2, b, l5);
 	st_printf("TEST HEX2 : '%10x' %06hx %hx '%-20lx'\n", a2, sh1, sh2, l5);
 	printf("TEST HEX2 : '%10x' %06hx %hx '%-20lx'\n", a2, sh1, sh2, l5);
-	
+
 	st_printf("TEST STRING :'%s';'%10s';'%-10s'\n", string, string, string);
 	printf("TEST STRING :'%s';'%10s';'%-10s'\n", string, string, string);
 	st_printf("TEST IP1    : %I-%10I : [%-20N - %20B]\n", s1, s2, s2, s2);
 	st_printf("TEST IP2    : %P-%20P : [%-20N - %20B]\n", s2, s2, s2, s2);
-	st_printf("TEST MASK   : %N : %m - %M \n", s1, s1, s1);
-	
+	st_printf("TEST MASK   : %N : %m - %M\n", s1, s1, s1);
+
 	st_snprintf(buffer1, sizeof(buffer1), "abcdefghijk");
 	printf("TRUNC1 : '%s'\n", buffer1);
 	snprintf(buffer1, sizeof(buffer1), "abcdefghijk");
