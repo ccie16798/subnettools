cacbb452c3a66e46a6159707ac938051868353ea
Etienne Basset <etienne.basset@ensta.org>
2018-07-09 11:55:55 +0200

    [STATS] subnet stats
    Signed-off-by: Etienne Basset <etienne.basset@ensta.org>

--- src/st_stats.c
+++ src/st_stats.c
@@ -33,9 +33,9 @@ unsigned int hash_subnet(void *key, int len)
 {
 	struct subnet *a = key;
 
-	if (a->ip_addr.ip_ver == IPV4_A)
+	if (a->ip_addr.ip_ver == IPV4_A || a->ip_addr.ip_ver == IPV4_N)
 		return djb_hash(&a->ip_addr.ip, 4) * a->mask;
-	if (a->ip_addr.ip_ver == IPV6_A)
+	if (a->ip_addr.ip_ver == IPV6_A || a->ip_addr.ip_ver == IPV6_N)
 		return djb_hash(&a->ip_addr.ip6, 16) * a->mask;
 	fprintf(stderr, "cannot hash unknown ip version %d\n", a->ip_ver);
 	return 0;
@@ -48,27 +48,34 @@ int ipam_stats(struct ipam_file *ipam, const char *statvalue)
 	struct hash_table ht;
 	struct stat_bucket *sb;
 	struct ipam_ea *ea;
+	struct subnet *subnet;
 	st_list head;
 
-	if (!strcmp(statvalue, "subnet"))
+	if (!strcmp(statvalue, "subnet")) {
 		res = alloc_hash_tab(&ht, ipam->nr, &hash_subnet);
-	else
-		res = alloc_hash_tab(&ht, ipam->nr, &djb_hash);
-	if (res < 0)
-		return res;
-	for (i = 0; i < ipam->ea_nr; i++) {
-		if (!strcmp(statvalue, ipam->ea[i].name))
-			ea_index = i;
-	}
-	if (ea_index < 0) {
-		fprintf(stderr, "unknown EA '%s'\n", statvalue);
-	       return -1;
-	}
+		if (res < 0)
+			return res;
 
-	for (i = 0; i < ipam->nr; i++) {
-		ea = &ipam->lines[i].ea[ea_index];
-		if (ea->value)
-			increase_key_stat(&ht, ea->value, ea->len);
+		for (i = 0; i < ipam->nr; i++) {
+			subnet = &ipam->lines[i].subnet;
+			if (subnet)
+				increase_key_stat(&ht, (char *)subnet, sizeof(subnet));
+		}
+	} else {
+		res = alloc_hash_tab(&ht, ipam->nr, &djb_hash);
+		for (i = 0; i < ipam->ea_nr; i++) {
+			if (!strcmp(statvalue, ipam->ea[i].name))
+				ea_index = i;
+		}
+		if (ea_index < 0) {
+			fprintf(stderr, "unknown EA '%s'\n", statvalue);
+			return -1;
+		}
+		for (i = 0; i < ipam->nr; i++) {
+			ea = &ipam->lines[i].ea[ea_index];
+			if (ea->value)
+				increase_key_stat(&ht, ea->value, ea->len);
+		}
 	}
 	init_list(&head);
 	sort_stat_table(&ht, &head);
