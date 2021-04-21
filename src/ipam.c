e227c34552298c9ee62d200d04148be6f3c989ca
Etienne <etienne.basset@loreal.com>
2021-04-21 12:47:36 +0200

    [IAPM] dont depend on EA name anymore
    Signed-off-by: Etienne <etienne.basset@loreal.com>

--- src/ipam.c
+++ src/ipam.c
@@ -126,7 +126,7 @@ static int ipam_ea_handle(char *s, void *data, struct csv_state *state)
 	* we  have csv_id 0 & 1 that are set for prefix and MASK, csv_id 2.... will handle EA
 	*/
 	ea_nr = state->csv_id - IPAM_STATIC_REGISTERED_FIELDS;
-	debug(IPAM, 6, "Found ea#%d %s = %s\n",  ea_nr, sf->lines[sf->nr].ea[ea_nr].name, s);
+	debug(IPAM, 6, "Found ea#%d %s = %s\n",  ea_nr, sf->ea[ea_nr].name, s);
 	/* we dont care if memory failed on strdup; we continue */
 	ea_strdup(&sf->lines[sf->nr].ea[ea_nr], s);
 	return CSV_VALID_FIELD;
